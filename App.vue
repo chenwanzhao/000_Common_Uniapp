eb1eec0246a92e192147c40cc65a294e548637fd
陈万照 <207668802@qq.com>
2020-07-30 14:09:03 +0800

    1.0.1

--- App.vue
+++ App.vue
@@ -4,28 +4,23 @@
  * 应用生命周期仅可在App.vue中监听，在其它页面监听无效
  -->
 <script>
+// 计步器
+// 注册计步器
+let step = uni.requireNativePlugin('DC-StepCounter');
+// 注册globalEvent
+let globalEvent = uni.requireNativePlugin('globalEvent');
 export default {
   /**
    * 当uni-app 初始化完成时触发（全局只触发一次）
    */
   onLaunch: function() {
     console.log('App Launch');
-    // 注册计步器
-    var step = uni.requireNativePlugin('DC-StepCounter');
-    // 注册globalEvent
-    var globalEvent = uni.requireNativePlugin('globalEvent');
+    
+    // Android计步器
     // 监听globalEvent事件 StepCounter_Ready 在ready后调用计步器相关API
-    globalEvent.addEventListener('StepCounter_Ready', function(e) {
-      step.getCurrentTimeSportStep(function(n) {
-        console.log('走了' + n + '步');
-      });
-      // 2019-01-13 为时间 使用者根据当前时间修改
-      step.getTodaySportStepArrayByDate('2020-07-28', function(array) {
-        console.log(JSON.stringify(array));
-      });
-    });
-    //调用初始化 先注册StepCounter_Ready事件 再调用initialize初始化 防止StepCounter_Ready事件丢失
-    step.initialize();
+    globalEvent.addEventListener('StepCounter_Ready', function(e) {});
+    // 调用初始化 先注册StepCounter_Ready事件 再调用initialize初始化 防止StepCounter_Ready事件丢失
+    step.initialize(); 
   },
 
   /**
@@ -33,6 +28,11 @@ export default {
    */
   onShow: function() {
     console.log('App Show');
+    
+    // Android计步器
+    step.getCurrentTimeSportStep(function(n) {
+      console.log('走了' + n + '步');
+    });
   },
 
   /**
